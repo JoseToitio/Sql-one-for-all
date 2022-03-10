bb53d96ae619f2b12c3f2db77362b0105f854572
Toitio <79183001+JoseToitio@users.noreply.github.com>
2022-03-10 02:53:21 -0300

    Delete desafio6.sql

--- desafio6.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-MIN(planos.valor) AS 'faturamento_minimo',
-ROUND(MAX(planos.valor), 2) AS 'faturamento_maximo',
-ROUND(AVG(planos.valor), 2) AS 'faturamento_medio',
-ROUND(SUM(planos.valor), 2) AS 'faturamento_total'
-FROM SpotifyClone.planos AS payment_plans
-JOIN SpotifyClone.user ON planos.id_plano = user.plano_id;
