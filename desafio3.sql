50f613db31154eaf5c2f16ea7ddd54fd321e984b
Toitio <josetoitio@gmail.com>
2022-03-10 02:11:10 -0300

    fix desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,8 @@
 SELECT user.username AS 'usuario',
 COUNT(DISTINCT(cancoes.cancao_name)) AS 'qtde_musicas_ouvidas',
 ROUND(SUM(cancoes.cancao_time) / 60, 2) AS 'total_minutos'
-FROM SpotifyClone.user JOIN SpotifyClone.history ON history.username = user.username
+FROM SpotifyClone.user 
+JOIN SpotifyClone.history ON history.id_user_follow = user.id_user_follow
 JOIN SpotifyClone.cancoes ON cancoes.id_cancao = history.id_cancao
 GROUP BY user.username
 ORDER BY user.username;
