a10a100594bf3c18bb9ac08b2d4d9f73883f9861
Toitio <josetoitio@gmail.com>
2022-03-09 22:35:49 -0300

    desafio 2 completo

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE SpotifyClone;
-USE SpotifyClone;
+
 CREATE TABLE SpotifyClone.planos (
   id_plano INTEGER PRIMARY KEY AUTO_INCREMENT,
   name_plano CHAR(50) NOT NULL,
--- desafio2.sql
+++ desafio2.sql
@@ -1,3 +1,3 @@
 SELECT (SELECT COUNT(*) FROM SpotifyClone.cancoes) AS cancoes,
-(SELECT COUNT(*) FROM SpotifyClone.artistas) AS artistas,
+(SELECT COUNT(*) FROM SpotifyClone.artist) AS artistas,
 (SELECT COUNT(*) FROM SpotifyClone.albums) AS albums;
