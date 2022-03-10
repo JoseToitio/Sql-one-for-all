7947642a4e1ef5d96fbdddec16b69520f3859c56
Toitio <josetoitio@gmail.com>
2022-03-10 03:26:45 -0300

    fix desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT
-CAST(MIN(planos.valor)) AS 'faturamento_minimo',
-CAST(MAX(planos.valor) AS DECIMAL(5,2)) AS 'faturamento_maximo',
-CAST(AVG(planos.valor), AS DECIMAL(5,2)) AS 'faturamento_medio',
-CAST(SUM(planos.valor), AS DECIMAL(5,2)) AS 'faturamento_total'
-FROM SpotifyClone.planos
+CAST(MIN(planos.valor) AS DECIMAL(5,2)) AS faturamento_minimo,
+CAST(MAX(planos.valor) AS DECIMAL(5,2)) AS faturamento_maximo,
+CAST(AVG(planos.valor) AS DECIMAL(5,2)) AS faturamento_medio,
+CAST(SUM(planos.valor) AS DECIMAL(5,2)) AS faturamento_total
+FROM SpotifyClone.planos AS planos
 JOIN SpotifyClone.user ON planos.id_plano = user.plano_id;
