8515379d6b026717fd941064379e1cb5848d341d
Toitio <josetoitio@gmail.com>
2022-03-09 23:45:27 -0300

    fix error desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -3,59 +3,57 @@ DROP DATABASE IF EXISTS SpotifyClone;
 CREATE DATABASE SpotifyClone;
 
 CREATE TABLE SpotifyClone.planos(
-  id_plano INTEGER PRIMARY KEY AUTO_INCREMENT,
+  id_plano INT PRIMARY KEY AUTO_INCREMENT,
   name_plano CHAR(50) NOT NULL,
   valor DOUBLE NOT NULL
 ) engine = InnoDB;
-
-CREATE TABLE SpotifyClone.user(
-  id_user INTEGER PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(45) NOT NULL,
-  id_user_follow INTEGER NOT NULL,
-  FOREIGN KEY (id_user_follow) REFERENCES planos(id_plano),
-  idade INT NOT NULL,
-  signature_date DATETIME DEFAULT NOW(),
-) engine = InnoDB;
-
 CREATE TABLE SpotifyClone.artist(
-  id_artist INTEGER PRIMARY KEY AUTO_INCREMENT,
+  id_artist INT PRIMARY KEY AUTO_INCREMENT,
   artist_name VARCHAR(45) NOT NULL
 ) engine = InnoDB;
-
 CREATE TABLE SpotifyClone.albums(
-    id_album INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_album INT PRIMARY KEY AUTO_INCREMENT,
     album_name VARCHAR(50) NOT NULL,
-    id_artista INTEGER NOT NULL,
+    id_artista INT NOT NULL,
     FOREIGN KEY (id_artista) REFERENCES artist(id_artist)
     ano_lancamento YEAR NOT NULL,
 ) engine = InnoDB;
 
-CREATE TABLE SpotifyClone.follow_artists(
-  id_user INTEGER NOT NULL,
-  id_artist INTEGER NOT NULL,
-  PRIMARY KEY (id_user, id_artist),
-  FOREIGN KEY (id_user) REFERENCES user(id_user),
-  FOREIGN KEY (id_artist) REFERENCES artist(id_artist)
+CREATE TABLE SpotifyClone.user(
+  id_user INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(45) NOT NULL,
+  id_user_follow INT NOT NULL,
+  FOREIGN KEY (id_user_follow) REFERENCES planos(id_plano),
+  idade INT NOT NULL,
+  signature_date DATETIME DEFAULT NOW(),
 ) engine = InnoDB;
-
 CREATE TABLE SpotifyClone.cancoes(
   id_cancao INTEGER PRIMARY KEY AUTO_INCREMENT,
   cancao_name VARCHAR(50) NOT NULL,
-  id_album INTEGER NOT NULL,
+  id_album INT NOT NULL,
   FOREIGN KEY (id_album) REFERENCES albums(id_album),
-  id_artist INTEGER NOT NULL,
+  id_artist INT NOT NULL,
   FOREIGN KEY (id_artist) REFERENCES artist(id_artist),
-  cancao_time INT NOT NULL DEFAULT 0
+  cancao_time INT
 ) engine = InnoDB;
-
 CREATE TABLE SpotifyClone.history(
-  id_user INTEGER NOT NULL,
-  id_cancao INTEGER NOT NULL,
+  id_user INT NOT NULL,
+  id_cancao INT NOT NULL,
   PRIMARY KEY (id_user, id_cancao),
   FOREIGN KEY (id_user) REFERENCES user(id_user),
   FOREIGN KEY (id_cancao) REFERENCES cancoes(id_cancao),
   history_date TIMESTAMP NOT NULL,
 ) engine = InnoDB;
+
+CREATE TABLE SpotifyClone.follow_artists(
+  id_user INT NOT NULL,
+  id_artist INT NOT NULL,
+  PRIMARY KEY (id_user, id_artist),
+  FOREIGN KEY (id_user) REFERENCES user(id_user),
+  FOREIGN KEY (id_artist) REFERENCES artist(id_artist)
+) engine = InnoDB;
+
+
 
 INSERT INTO SpotifyClone.planos (name_plano, valor) 
 VALUES
