0534e4066d0051659d6397aa1ce07a65c853e0bd
Toitio <josetoitio@gmail.com>
2022-03-10 04:02:28 -0300

    desafio 8 completo

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
 SELECT artist.artist_name AS artista,
 albums.album_name AS album
 FROM SpotifyClone.artist
-JOIN SpotifyClone.albums ON artist.id_artista = albums.id_artista
+JOIN SpotifyClone.albums ON artist.id_artist = albums.id_artista
 LIMIT 2;
