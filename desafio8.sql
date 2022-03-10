471b21b1f80b3b05fd751538a873350214f346fa
Toitio <josetoitio@gmail.com>
2022-03-10 03:53:54 -0300

    desafio 8 completo

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,5 @@
 SELECT artist.artist_name AS artista,
 albums.album_name AS album,
 FROM SpotifyClone.artist
-JOIN SpotifyClone.albums ON albums.id_artista = artist.id_artist
-WHERE artist_name = 'Walter Phoenix'
-ORDER BY album;
+JOIN SpotifyClone.albums ON artist.id_artista = albums.id_artista
+LIMIT 2;
